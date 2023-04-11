8257d87c90c89852abb0297632980948fe2d66a4
Harsh Mathur <harsh@felicity.care>
2023-04-11 17:32:35 +0530

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xFc5a6601eE821B20E6e5813C2260fa4d750682bc";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
